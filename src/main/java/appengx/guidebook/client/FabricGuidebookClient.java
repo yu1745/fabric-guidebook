71bc59677a53037f2988b0242490bc7a9be7b63f
yu1745 <43715680+yu1745@users.noreply.github.com>
2026-05-30 19:51:37 +0800

    Add guidebook structure authoring commands

--- src/main/java/appengx/guidebook/client/FabricGuidebookClient.java
+++ src/main/java/appengx/guidebook/client/FabricGuidebookClient.java
@@ -1,9 +1,11 @@
 package appengx.guidebook.client;
 
+import appengx.client.guidebook.command.GuidebookStructureCommands;
 import appengx.guidebook.api.Guidebooks;
 import appengx.guidebook.FabricGuidebook;
 import appengx.guidebook.api.Guide;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.server.packs.PackType;
 
@@ -18,5 +20,7 @@ public final class FabricGuidebookClient implements ClientModInitializer {
         Guidebooks.setOpenHandler(ClientGuidebooks::open);
         ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(new GuideResourceReloader());
         OpenGuideHotkey.init();
+        ServerLifecycleEvents.SERVER_STARTING.register(server -> GuidebookStructureCommands.register(
+                server.getCommands().getDispatcher()));
     }
 }
